0c8c249564946a819405308dca40573270722a68
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2022-04-15 12:26:12 +0700

    limit column width to 80 - part I

--- 2021-22/2022-04-14_132p_41lines.py
+++ 2021-22/2022-04-14_132p_41lines.py
@@ -1,4 +1,4 @@
-# 130 points - 41 lines - 30 seconds - 14.04.2022
+# 130 points - 41 lines - 30 seconds - 15.04.2022
 from vexcode_vrc import *
 from math import sqrt
 path = [[-920,  920, 0],[-920,-1450, 0],[750, -1400, 0],[ 500,-1150, 1],[-600, -710, 0],
@@ -15,17 +15,17 @@ def goto(target_x, target_y, reverse):
     if ( delta_x == 0 ):            # can't divide by zero, vertical motion
         direction = 90              # standard: drive upwards
         if ( delta_y > 0):
-            direction = 270         # otherwise down
+            direction = 270         # otherwise downwards
     else:
         direction = - math.atan(delta_y / delta_x) * 180 / math.pi
-    if ( delta_x < 0 ):             # atan range is [-180|180], but we need [0|360] 
+    if ( delta_x < 0 ):             # atan range is [-180|180] but need [0|360] 
         direction += 180
-    if ( reverse is 1 ):            # driving reverse in oposite calculated direction
+    if ( reverse is 1 ):            # driving reverse in opposite direction
         direction += 180
         direction_fr = REVERSE
-    direction = direction % 360     # with the modulo % operator, range < 360 degrees
-    drivetrain.turn_to_heading(direction, DEGREES, wait=True)
-    drivetrain.drive_for(direction_fr, distance, MM, wait=True)
+    direction = direction % 360     # with the modulo % operator, range < 360 
+    drivetrain.turn_to_heading(direction, DEGREES, wait=True)   # (1) DIRECTION
+    drivetrain.drive_for(direction_fr, distance, MM, wait=True) # (2) DRIVING
 def pick_up():
     fork_motor_group.spin_to_position(1500, DEGREES, wait=True)
 def set_down():
